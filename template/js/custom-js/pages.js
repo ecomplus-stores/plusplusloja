2b940f0a4eebb756143651455612055318b82624
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-12-05 01:15:38 -0300

    chore: set pages with affiliate

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,9 @@
 // Add your custom JavaScript for storefront pages here.
+import Vue from 'vue'
+
+const affiliateLinkDiv = document.getElementById('affiliate-link')
+if (affiliateLinkDiv) {
+  import('./components/AffiliateLink.vue').then(({ default: AffiliateLink }) => {
+    new Vue(AffiliateLink).$mount(affiliateLinkDiv)
+  })
+}
